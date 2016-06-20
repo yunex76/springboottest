be57a7a3bc867361bf8bca24401c4562393a067b
yunex76 <yunex76@gmail.com>
2016-06-20 17:24:59 +0900

    데이터 소스 설정을 명시적으로 변경하기 (page 63 적용)

--- hajibook-jdbc/src/main/resources/data.sql
+++ hajibook-jdbc/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO customers(first_name, last_name) VALUES ( 'Nobita', 'Nobi');
+INSERT INTO customers(first_name, last_name) VALUES ( '길동', '홍');
 INSERT INTO customers(first_name, last_name) VALUES ( 'Takeshi', 'Goda');
 INSERT INTO customers(first_name, last_name) VALUES ( 'Suneo', 'Honekawa');
 INSERT INTO customers(first_name, last_name) VALUES ( 'Shizuka', 'Minamoto');
