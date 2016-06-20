6ef662b7ac3dea5af5c221b32d097307daa275df
yunex76 <yunex76@gmail.com>
2016-06-20 16:11:42 +0900

    first commit

---
+++ hajibook-jdbc/src/main/java/com/example/App.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+@EnableAutoConfiguration
+public class App implements CommandLineRunner {
+	
+	@Autowired
+	NamedParameterJdbcTemplate jdbcTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		String sql = "SELECT 1";
+		SqlParameterSource param = new MapSqlParameterSource();
+		Integer result = jdbcTemplate.queryForObject(sql, param, Integer.class);
+		
+		System.out.println("result = " + result);
+	}
+}
