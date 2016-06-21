9c6ee498a802d77ba3311ab0aa6666e2ed28837d
yunex76 <yunex76@gmail.com>
2016-06-21 16:31:09 +0900

    1. page 처리

--- hajibook-jpa/src/main/java/com/example/App.java
+++ hajibook-jpa/src/main/java/com/example/App.java
@@ -5,6 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import com.example.domain.Customer;
 import com.example.repository.CustomerRepository;
@@ -31,10 +34,13 @@ public class App implements CommandLineRunner {
 		created = customerRepository.save(new Customer(null, "Shinken", "Takeda"));
 		System.out.println(created + " is created!");
 		
-		// 데이터 표시
-		customerRepository.findAll().forEach(System.out::println);
-
-		// 데이터 표시
-		customerRepository.findAllOrderByName().forEach(System.out::println);
+		// 페이징 처리
+		Pageable pageable = new PageRequest(0, 3);
+		Page<Customer> page = customerRepository.findAll(pageable);
+		System.out.println("한 페이지당 데이터 수 = " + page.getSize());
+		System.out.println("현재 페이지 = " + page.getNumber());
+		System.out.println("전체 페이지 수 = " + page.getTotalPages());
+		System.out.println("전체 데이터 수 = " + page.getTotalElements());
+		page.getContent().forEach(System.out::println);
 	}
 }
