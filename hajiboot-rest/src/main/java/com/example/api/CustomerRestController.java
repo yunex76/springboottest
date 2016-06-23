8565575f70322ede45151b646425428adeac6ad0
yunex76 <yunex76@gmail.com>
2016-06-23 13:27:58 +0900

    3.2.2. 신규 고객 정보 작성, 고객 한명의 정보 업데이트, 고객 한명의 정보 삭제 API 구현 (PAGE 099)

--- hajiboot-rest/src/main/java/com/example/api/CustomerRestController.java
+++ hajiboot-rest/src/main/java/com/example/api/CustomerRestController.java
@@ -3,9 +3,12 @@ package com.example.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.domain.Customer;
@@ -29,5 +32,26 @@ public class CustomerRestController {
 	Customer getCustomer(@PathVariable Integer id) {
 		Customer customer = customerService.findOne(id);
 		return customer;
+	}
+	
+	// 신규 고객 정보 작성
+	@RequestMapping(method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	Customer postCustomers(@RequestBody Customer customer) {
+		return customerService.create(customer);
+	}
+	
+	// 고객 한명의 정보 업데이트
+	@RequestMapping(value = "{id}", method = RequestMethod.PUT)
+	Customer putCustomer(@PathVariable Integer id, @RequestBody Customer customer) {
+		customer.setId(id);
+		return customerService.update(customer);
+	}
+	
+	// 고객 한명의 정보 삭제
+	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	void deleteCustomer(@PathVariable Integer id) {
+		customerService.delete(id);
 	}
 }
