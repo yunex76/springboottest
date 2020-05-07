e392d286dc44a5205cf08da5593d3e81c6617d6e
yunex <yunex76@gmail.com>
2020-05-07 14:47:13 +0900

    init commit

---
+++ thymeleaf-test/src/main/java/com/yunex/springboot/controller/UserTestController.java
@@ -0,0 +1,18 @@
+package com.yunex.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.yunex.springboot.entity.User;
+
+@Controller
+public class UserTestController {
+
+    @GetMapping("/test")
+    public String getUser(Model model) {
+        User user = new User("kkaok", "테스트", "web") ;
+        model.addAttribute("user", user);
+        return "test";
+    }	
+}
